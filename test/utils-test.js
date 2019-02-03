2ce887b4d913e6fd5c53a170bdd19da6b58b52ad
Justin Torre <justintorre75@gmail.com>
2019-02-03 14:32:46 -0500

    Added functionality to display all the exemplars in order. as well as added a template for instructions. Modified some tests as well

--- test/utils-test.js
+++ test/utils-test.js
@@ -16,18 +16,22 @@ let utils = require('../utils.js')
 
 
 describe('Image testing', function() {
-    describe('GeneratingImageHTML test', function() {
-      const exemplar0 = new utils.Exemplar('NNN');
-      exemplar0.images[0].fileName = '2101.jpg';
-      const image0 = exemplar0.images[0];
+  describe('GeneratingImageHTML test', function() {
+    const exemplar0 = new utils.Exemplar('NNN');
+    exemplar0.images[0].fileName = '2101.jpg';
+    const image0 = exemplar0.images[0];
+    it('This is a test that basically has copy and pasted code.', function(){
+      assert.equal(utils.generateImageHTML(image0),
+      `<img class="stimulus-image" src="assets/stimuli/neutral/2101.jpg" style="max-width:75%;">`)
+    });
+    it('This is a test that basically has copy and pasted code.', function(){
+      assert.equal(utils.generateImageHTML(utils.exemplars['NNN1'].images[0]),
+      `<img class="stimulus-image" src="${utils.getImagePath(utils.exemplars['NNN1'].images[0])}" style="max-width:75%;">`)
+    });
 
-      it('This is a test that basically has copy and pasted code.', function(){
-        assert.equal(utils.generateImageHTML(image0),
-        `<img class="stimulus-image" src="assets/stimuli/neutral/2101.jpg" style="max-width:75%;">`)
-      });
-      it('This is a test that basically has copy and pasted code.', function(){
-        assert.equal(utils.generateImageHTML(utils.exemplars['NNN1'].images[0]),
-        `<img class="stimulus-image" src="${utils.getImagePath(utils.exemplars['NNN1'].images[0])}" style="max-width:75%;">`)
-      });
+    it('This is a test that basically has copy and pasted code.', function(){
+      assert.equal(utils.generateImageHTML(utils.exemplars['NNN1'].images[0]),
+      `<img class="stimulus-image" src="${utils.getImagePath(utils.exemplars['NNN1'].images[0])}" style="max-width:75%;">`)
     });
   });
+});

15b9d5cb7ae877e69601bfb95937356290338de7
Ryan Drew <ryan.drew14@gmail.com>
2019-02-04 01:17:02 -0500

    adding framework for grey dots, and adding displaying of images in correct order

--- utils.js
+++ utils.js
@@ -1,15 +1,32 @@
+/* CONSTANTS */
+
+const param = {};
+param['img_x'] = 640;
+param['img_y'] = 480;
+param['grey_radius'] = 25;
+param['display_time'] = 1;
+param['fixation_time'] = 0.5;
+param['trialsPerEncodingBlock'] = 144;
+param['encodingBlocks'] = 6;
+
+const whiteDotTrial = {
+  type: 'html-keyboard-response',
+  stimulus: '<p>hi</p>',
+  trial_duration: param['fixation_time'],
+};
+
 // Utilities for use in the html scripts
 const neuImages = ['1908.jpg', '2101.jpg', '2191.jpg', '1945.jpg', '1390.jpg', '2520.jpg', '2107.jpg', '2393.jpg', '2484.jpg', '2597.jpg', '2020.jpg', '1903.jpg', '2309.jpg', '1645.jpg', '1114.jpg', '2635.jpg', '2272.jpg', '1302.jpg', '1122.jpg', '2383.jpg', '2359.jpg', '2840.jpg', '2575.jpg', '2122.jpg', '2890.jpg', '2220.jpg', '2411.jpg', '1617.jpg', '1670.jpg', '2384.jpg', '2749.jpg', '1935.jpg', '2279.jpg', '2397.jpg', '2210.jpg', '2377.jpg', '2579.jpg', '2458.jpg', '2445.jpg', '2308.jpg', '2446.jpg', '1560.jpg', '2032.jpg', '2206.jpg', '2221.jpg', '2752.jpg', '1947.jpg', '1931.jpg', '2435.jpg', '2102.jpg', '2235.jpg', '2396.jpg', '1230.jpg', '2215.jpg', '2695.jpg', '2745.1.jpg', '2521.jpg', '2870.jpg', '1726.jpg', '1350.jpg', '2704.jpg', '1820.jpg', '1675.jpg', '2606.jpg', '1616.jpg', '2770.jpg', '2850.jpg'];
 const negImages = ['9909.jpg', '7380.jpg', '9530.jpg', '3261.jpg', '6315.jpg', '9571.jpg', '9412.jpg', '2456.jpg', '9220.jpg', '3019.jpg', '2352.2.jpg', '9187.jpg', '2811.jpg', '9006.jpg', '9183.jpg', '9927.jpg', '2688.jpg', '9414.jpg', '3350.jpg', '3212.jpg', '3001.jpg', '9280.jpg', '3170.jpg', '9140.jpg', '9181.jpg', '2301.jpg', '2799.jpg', '2800.jpg', '3230.jpg', '6312.jpg', '6821.jpg', '3550.1.jpg', '9043.jpg', '2683.jpg', '9561.jpg', '3300.jpg', '3053.jpg', '2053.jpg', '3180.jpg', '2751.jpg', '9600.jpg', '9570.jpg', '9295.jpg', '9410.jpg', '6825.jpg', '3195.jpg', '3160.jpg', '9342.jpg', '2345.1.jpg', '9810.jpg', '3064.jpg', '9265.jpg', '2141.jpg', '9185.jpg', '3120.jpg', '9413.jpg', '6563.jpg', '9325.jpg', '9635.1.jpg', '9041.jpg', '6243.jpg', '9921.jpg', '9831.jpg', '3015.jpg', '9560.jpg', '6571.jpg', '2710.jpg', '2205.jpg', '9415.jpg', '2900.jpg', '9520.jpg', '2730.jpg', '9424.jpg', '9800.jpg', '3400.jpg', '9301.jpg', '9332.jpg', '9075.jpg', '9254.jpg'];
 
-/**
- * Returns an html tag to display the image.
- * @param {*} type 'N' for a neutral image, 'B' for a negative one
- * @param {*} current a list of the currently used images
- */
-const generateImageHTML = (image) => {
-  return `<img class="stimulus-image" src="${getImagePath(image)}" style="max-width:75%;">`;
-};
+/*
+IMAGE OBJECTS
+{
+  fileName: String representing file name (without path),
+  valence: 'B' or 'N',
+  dotPlacement: 'right' or 'left',
+}
+*/
 
 /**
  * Returns a random image with the given type, and mutates the list given to ensure that
@@ -45,7 +62,27 @@ const randomlyPickBetween = (one, two) => {
   return random <= 0.5 ? one : two;
 };
 
+/**
+ * Picks randomly an element of the given list.
+ * @param {*} list the list from which to pick
+ */
+const randomlyPickFromList = list => list[Math.floor(Math.random() * list.length)];
+
 const getImagePath = ({ valence, fileName }) => `assets/stimuli/${valence === 'B' ? 'negative' : 'neutral'}/${fileName}`;
+
+/**
+ * Returns an html tag to display the image.
+ * @param {*} type 'N' for a neutral image, 'B' for a negative one
+ * @param {*} current a list of the currently used images
+ */
+const generateImageHTML = image => `
+<div class="img-overlay-wrap">
+<img src="${getImagePath(image)}">
+<svg height="${param['img_x']}" width="${param['img_y']}">
+  <circle cx="70" cy="70" r="${param['grey_radius']}" fill="#a9a9a9" fill-opacity="0.4" />
+</svg> 
+</div>
+`;
 
 // N = neutral, B = negative
 const tripletTypes = ['NNN', 'NNB', 'BNN', 'BBB'];
@@ -53,6 +90,14 @@ const tripletTypes = ['NNN', 'NNB', 'BNN', 'BBB'];
 // current list of used stimuli
 const currentList = [];
 
+const removeElement = (array, element) => {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === element) {
+      return array.splice(i, 1);
+    }
+  }
+  return null;
+};
 
 const copyImage = img => ({
   filename: img.fileName,
@@ -111,7 +156,7 @@ class Exemplar {
 }
 
 /*
-Provided variables:
+Provided variable:
 exemplars
   - object with string keys
   - keys are "NNN1", "NNN2", "NBB1", "NBB2", etc to match tripletTypes (2 per triplet)
@@ -125,14 +170,24 @@ for (let i = 0; i < tripletTypes.length; i += 1) {
   exemplars[`${type}2`] = exemplar2;
 }
 
+const createExemplarCounts = () => {
+  const exemplarCounts = {};
+  for (let i = 0; i < tripletTypes.length; i += 1) {
+    const type = tripletTypes[i];
+    exemplarCounts[`${type}1`] = 6;
+    exemplarCounts[`${type}2`] = 6;
+  }
+  return exemplarCounts;
+};
 
 /**
  * Export modules for testing purposes
  */
 if (typeof module !== 'undefined' && module.exports != null) {
   module.exports = {
-    generateImageHTML, 
+    generateImageHTML,
     Exemplar,
-    exemplars, 
-    getImagePath};
+    exemplars,
+    getImagePath,
+  };
 }

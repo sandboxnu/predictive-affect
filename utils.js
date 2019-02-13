ff721eab48b157be982ae23695f40074334a93da
Ryan Drew <ryan.drew14@gmail.com>
2019-02-12 20:10:26 -0500

    add normalization for setup data

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@ param.grey_radius = 25;
 param.display_time = 1.5;
 param.fixation_time = 0.5;
 param.trialsPerEncodingBlock = 144;
-param.encodingBlocks = 6;
+param.encodingBlocks = 1;
 
 // Utilities for use in the html scripts
 const neuImages = ['1908.jpg', '2101.jpg', '2191.jpg', '1945.jpg', '1390.jpg', '2520.jpg', '2107.jpg', '2393.jpg', '2484.jpg', '2597.jpg', '2020.jpg', '1903.jpg', '2309.jpg', '1645.jpg', '1114.jpg', '2635.jpg', '2272.jpg', '1302.jpg', '1122.jpg', '2383.jpg', '2359.jpg', '2840.jpg', '2575.jpg', '2122.jpg', '2890.jpg', '2220.jpg', '2411.jpg', '1617.jpg', '1670.jpg', '2384.jpg', '2749.jpg', '1935.jpg', '2279.jpg', '2397.jpg', '2210.jpg', '2377.jpg', '2579.jpg', '2458.jpg', '2445.jpg', '2308.jpg', '2446.jpg', '1560.jpg', '2032.jpg', '2206.jpg', '2221.jpg', '2752.jpg', '1947.jpg', '1931.jpg', '2435.jpg', '2102.jpg', '2235.jpg', '2396.jpg', '1230.jpg', '2215.jpg', '2695.jpg', '2745.1.jpg', '2521.jpg', '2870.jpg', '1726.jpg', '1350.jpg', '2704.jpg', '1820.jpg', '1675.jpg', '2606.jpg', '1616.jpg', '2770.jpg', '2850.jpg'];
@@ -116,6 +116,10 @@ const showFixationDot = (timeline) => {
   timeline.push(whiteDot);
 };
 
+const pickRandomBetween = (lower, upper) => {
+  return Math.random() * (upper - lower) + lower;
+}
+
 /**
  * Exemplar is a set of 3 images also known as a triplet.
  */
@@ -161,15 +165,17 @@ class Exemplar {
     const { type, images } = this;
     for (let charIndex = 0; charIndex < type.length; charIndex += 1) {
       const imgValence = type.charAt(charIndex);
+      const dotSide = randomlyPickBetween('left', 'right');
       const image = {
         fileName: randomlySelectImage(imgValence, currentList),
         valence: imgValence,
-        greyDotX: Math.random() * (param.img_x - param.grey_radius * 2)
-                    + param.grey_radius,
-        greyDotY: Math.random() * (param.img_y - param.grey_radius * 2)
-                    + param.grey_radius,
+        dotPlacement: dotSide,
+        greyDotX: dotSide === 'left'
+          ? pickRandomBetween(param.grey_radius, param.img_x / 2 - param.grey_radius)
+          : pickRandomBetween(param.img_x / 2 + param.grey_radius, param.img_x - param.grey_radius),
+        greyDotY: pickRandomBetween(param.grey_radius, param.img_y - param.grey_radius),
+        positionInExemplar: charIndex,
       };
-      image.dotPlacement = image.greyDotX < param.img_x / 2 ? 'left' : 'right';
       images.push(image);
     }
   }
@@ -198,6 +204,21 @@ const createExemplarCounts = () => {
     exemplarCounts[`${type}2`] = 6;
   }
   return exemplarCounts;
+};
+
+// takes in exemplars and returns data nicely formatted for csv
+const normalizeExemplars = (exmps) => {
+  const data = {};
+  exmps.entries().forEach((exemplarEntry) => {
+    const exemplar = exemplarEntry[1];
+    exemplar.getImages().forEach((image) => {
+      data.push({
+        exemplarName: exemplarEntry[0],
+        type: exemplar.type,
+        ...image,
+      });
+    });
+  });
 };
 
 /**

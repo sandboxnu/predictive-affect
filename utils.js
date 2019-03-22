106830de774b2b1a8a02939dd1c262592af6bc99
Sarah Witt <witt.s@husky.neu.edu>
2019-03-21 21:39:26 -0400

    give participant their completion code

--- utils.js
+++ utils.js
@@ -7,12 +7,12 @@ param.img_y = 480;
 param.grey_radius = 25;
 param.display_time = 1.5;
 param.fixation_time = 0.5;
-param.trialsPerEncodingBlock = 144;
+param.trialsPerEncodingBlock = 1;
 param.encodingBlocks = 1;
 param.foilTestedOn = [1, 2, 0, 1, 2, 1, 2, 0, 2, 0, 1, 2, 1, 0];
 param.foilTestedType = [false, true, false, true, false, false,
   true, true, true, false, false, true];
-param.completitionCode = Math.floor(Math.random() * 1000000000);
+param.completionCode = Math.floor(Math.random() * 1000000000);
 
 // Utilities for use in the html scripts
 const neuImages = ['1908.jpg', '2101.jpg', '2191.jpg', '1945.jpg', '1390.jpg', '2520.jpg', '2107.jpg', '2393.jpg', '2484.jpg', '2597.jpg', '2020.jpg', '1903.jpg', '2309.jpg', '1645.jpg', '1114.jpg', '2635.jpg', '2272.jpg', '1302.jpg', '1122.jpg', '2383.jpg', '2359.jpg', '2840.jpg', '2575.jpg', '2122.jpg', '2890.jpg', '2220.jpg', '2411.jpg', '1617.jpg', '1670.jpg', '2384.jpg', '2749.jpg', '1935.jpg', '2279.jpg', '2397.jpg', '2210.jpg', '2377.jpg', '2579.jpg', '2458.jpg', '2445.jpg', '2308.jpg', '2446.jpg', '1560.jpg', '2032.jpg', '2206.jpg', '2221.jpg', '2752.jpg', '1947.jpg', '1931.jpg', '2435.jpg', '2102.jpg', '2235.jpg', '2396.jpg', '1230.jpg', '2215.jpg', '2695.jpg', '2745.1.jpg', '2521.jpg', '2870.jpg', '1726.jpg', '1350.jpg', '2704.jpg', '1820.jpg', '1675.jpg', '2606.jpg', '1616.jpg', '2770.jpg', '2850.jpg'];

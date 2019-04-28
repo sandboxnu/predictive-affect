73b90c1e023a92e7745c88dd8b3536466cba928e
Sarah Witt <witt.s@husky.neu.edu>
2019-04-27 23:08:59 -0400

    Add random image functionaity

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 /* CONSTANTS */
 
 const param = {};
+param.randomTriplets = true;
 param.exemplarTypes = ['NNN', 'NNB', 'BNN', 'BBB']; // the different types of exemplar. these can be triplets, pairs, etc
 param.numExemplarsPerType = 2; // number of exemplars per type (see exemplarTypes variable)
 param.imageStructLength = 3;
@@ -208,7 +209,7 @@ class Exemplar {
 
   // TODO both these methods should copy
   getImages() {
-    return this.images;
+    return !param.randomTriplets ? this.images.sort( (x, y) => Math.random() - 0.5): this.images;
   }
 
   getImageNames() {

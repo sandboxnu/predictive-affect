56b78719f675079a38060a4388e06da1aee5f7b5
Ryan Drew <ryan.drew14@gmail.com>
2019-06-23 09:36:04 -0400

    add eslint, yarn, prettier (WIP)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
-    "extends": "airbnb"
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
 };

062d7ca4d9a6283a75423397e0f2a084653a5421
Sarah Witt <witt.s@husky.neu.edu>
2019-07-07 12:29:38 -0400

    Default param

--- src/param.example.js
+++ src/param.example.js
@@ -1,7 +1,7 @@
 const param = {};
 param.randomTriplets = false; /* false if the images in the triplets are in the same order every
                                  block/phase, true if they are randomly shuffled before appearing */
-param.exemplarTypes = ["NNN", "BBB"]; // the different types of exemplar. these can be triplets, pairs, etc
+param.exemplarTypes = ["NNN", "NNB", "BNN", "BBB"]; // the different types of exemplar. these can be triplets, pairs, etc NNN, NNB, BNN, or BBB
 param.numExemplarsPerType = 2; // number of exemplars per type (see exemplarTypes variable)
 param.imageStructLength = (
   param.exemplarTypes[0] || []
@@ -12,24 +12,32 @@ param.grey_radius = 25; // radius of grey dot
 param.display_time = 1.5; // time in seconds to display encoding trial
 param.fixation_time = 0.5; // time in seconds to display fixation trial
 param.break_duration = 60; // time in seconds to have a break between blocks
-param.encodingBlocks = 1; // number of encoding blocks
-param.repPerBlock = 1; // number of repetitions per exemplar encoding block
+param.encodingBlocks = 6; // number of encoding blocks
+param.repPerBlock = 6; // number of repetitions per exemplar encoding block
 param.trialsPerEncodingBlock =
   param.exemplarTypes.reduce((acc, t) => acc + t.length, 0) *
   param.repPerBlock *
   param.exemplarTypes.length; // total number of trials per block
 param.foilTestedOn = [
-  1,
   0,
+  2,
   0,
-  1
+  2,
+  2,
+  2,
+  0,
+  0
 ]; /* for every exemplar, the index of the image which the foil is
       different from the original triplet */
 param.foilTestedType = [
   false,
   true,
   false,
-  true
+  true,
+  true,
+  false,
+  true,
+  false,
 ]; /* for every exemplar, whether the replaced image in the foil is the same affect as the image it replaces. */
 param.completionCode = Math.floor(Math.random() * 1000000000);
 
